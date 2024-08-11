f13a09c30c6db561ade568a4935e5a218cd94ad2
Arvik <94380788+arviik@users.noreply.github.com>
2024-08-12 01:15:11 +0200

    feat(fronted-user): Added method in properties service to retrieve the properties list of the connected landlord

--- frontend-user/src/api/services/properties.ts
+++ frontend-user/src/api/services/properties.ts
@@ -1,6 +1,8 @@
 import ky from "ky";
-import {ApiResponse} from "@/types";
+import {ApiResponse, User} from "@/types";
 import {Property} from "@/components/properties/Properties";
+import {authService} from "@/api/services/authService";
+import {api} from "@/api/config";
 
 export const propertiesService = {
     getPropertyById: async (id: number): Promise<ApiResponse<Property>> => {
@@ -9,5 +11,14 @@ export const propertiesService = {
 
     getProperties: async (): Promise<ApiResponse<Property[]>> => {
         return ky.get(`http://localhost:3000/properties`).json<ApiResponse<Property[]>>();
+    },
+
+    getPropertiesByUserId: async () => {
+        const user: User = await authService.getUserInfo();
+        if (user.Landlord?.id) {
+            const landlordId: number = user.Landlord.id;
+            return await api.get(`properties/landlord/${landlordId}`).json<ApiResponse<Property[]>>();
+        }
+        return new Promise<ApiResponse<Property[]>>(() => {});
     }
 }
