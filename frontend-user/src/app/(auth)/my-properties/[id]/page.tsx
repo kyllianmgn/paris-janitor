64017da3349681a6150825c24fbd4c9846b6b4be
Arvik <94380788+arviik@users.noreply.github.com>
2024-08-12 01:18:03 +0200

    feat(fronted-user): Added new route for allowing landlords to see a list of their properties

---
+++ frontend-user/src/app/(auth)/my-properties/[id]/page.tsx
@@ -0,0 +1,5 @@
+import {PropertyDetail} from "@/components/properties/PropertyDetail";
+
+export default function PropertiesRoute({params}: Readonly<{ params: { id: number } }>) {
+    return <PropertyDetail propertyId={params.id}></PropertyDetail>
+}
