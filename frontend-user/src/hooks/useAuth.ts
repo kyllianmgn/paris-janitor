ab84814a035902a48c89c984ae571c628ffdee6e
Kani <gkounkoud@myges.fr>
2024-08-18 11:36:15 +0200

    feat(frontend-user): Add signup functionality to useAuth hook

--- frontend-user/src/hooks/useAuth.ts
+++ frontend-user/src/hooks/useAuth.ts
@@ -1,21 +1,21 @@
 "use client";
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/store/store';
+import {AppDispatch, RootState, store} from '@/store/store';
 import { setCredentials, logout } from '@/store/slices/authSlice';
 import { authService } from '@/api/services/authService';
-import {LoginRequest, TokenResponse} from "@/types";
+import {LoginRequest, SignUpRequest, TokenResponse} from "@/types";
 import {tokenUtils} from "@/api/config";
 import {useEffect, useState} from "react";
 
 export const useAuth = () => {
-    const dispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch();
     const auth = useSelector((state: RootState) => state.auth);
     const [isLoading, setIsLoading] = useState(true);
 
     const login = async (loginData: LoginRequest) => {
         try {
             const response = await authService.login(loginData, dispatch);
-            dispatch(setCredentials(response));
+            store.dispatch(setCredentials(response));
         } catch (error) {
             throw error;
         }
@@ -24,6 +24,15 @@ export const useAuth = () => {
     const logoutUser = () => {
         authService.logout(dispatch);
         dispatch(logout());
+    };
+
+    const signup = async (signupData: SignUpRequest) => {
+        try {
+            const response = await authService.signup(signupData, dispatch);
+            store.dispatch(setCredentials(response));
+        } catch (error) {
+            throw error;
+        }
     };
 
     const checkAuth = async () => {
