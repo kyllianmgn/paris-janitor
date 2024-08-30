bebd224efcbabcf020ca2bdb39e62d8d4b6f49d2
Kani <gkounkoud@myges.fr>
2024-08-31 00:05:07 +0200

    chore(api): Add reset password functionality for users

--- frontend-admin/src/api/services/user-service.ts
+++ frontend-admin/src/api/services/user-service.ts
@@ -1,19 +1,29 @@
 import {api} from "@/api/config";
 import {ApiResponse, Landlord, ServiceProvider, Traveler, User} from "@/types";
 
-export const getUsers = async (query?: string, page?: number): Promise<ApiResponse<User[]>> => {
-    let searchParams = ""
-    if (query){
-        searchParams = `?query=${query}`
+export const getUsers = async (query?: string, page?: number, filters?: any): Promise<ApiResponse<User[]>> => {
+    let searchParams = new URLSearchParams();
+    if (query) {
+        searchParams.append("query", query);
     }
-    if (page){
-        if (searchParams == ""){
-            searchParams = `?page=${page}`
-        }else{
-            searchParams += `&page=${page}`
-        }
+    if (page) {
+        searchParams.append("page", page.toString());
     }
-    return await api.get(`users${searchParams}`).json();
+    if (filters) {
+        Object.entries(filters).forEach(([key, value]) => {
+            if (typeof value === 'object') {
+                Object.entries(value).forEach(([subKey, subValue]) => {
+                    if (subValue) {
+                        searchParams.append(`${key}[${subKey}]`, 'true');
+                    }
+                });
+            } else if (value) {
+                searchParams.append(key, 'true');
+            }
+        });
+    }
+    const url = `users?${searchParams.toString()}`;
+    return await api.get(url).json();
 }
 
 export const editUser = async (id: number, user: Pick<User, "firstName" | "lastName" | "email">): Promise<ApiResponse<User>> => {
@@ -26,4 +36,8 @@ export const getUserById = async (id: number): Promise<ApiResponse<User>> => {
 
 export const banUser = async (id: number, date: Date): Promise<ApiResponse<User>> => {
     return await api.patch(`users/${id}/ban`, {json: {date: date}}).json();
+}
+
+export const resetPassword = async (id: number): Promise<ApiResponse<User>> => {
+    return await api.post(`users/reset-password/${id}`).json();
 }
