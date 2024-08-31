bb617f6fe9a98e55788e36721543b4a1b7fe3b68
Kani <gkounkoud@myges.fr>
2024-08-31 13:08:25 +0200

    chore: Update subscription routes and components, add UserTable component for managing users

---
+++ frontend-admin/src/api/services/subscription-service.ts
@@ -0,0 +1,46 @@
+import { api } from "@/api/config";
+import {ApiResponse, Subscription, SubscriptionPlan, SubscriptionRequest} from "@/types";
+// Subscriptions
+
+export const getSubscriptions = async (): Promise<ApiResponse<Subscription[]>> => {
+    return await api.get('subscriptions').json();
+};
+
+export const getSubscription = async (id: number): Promise<ApiResponse<Subscription>> => {
+    return await api.get(`subscriptions/${id}`).json();
+};
+
+export const createSubscription = async (subscription: SubscriptionRequest): Promise<ApiResponse<Subscription>> => {
+    return await api.post('subscriptions', { json: subscription }).json();
+};
+
+export const updateSubscription = async (id: number, subscription: Partial<Subscription>): Promise<ApiResponse<Subscription>> => {
+    return await api.put(`subscriptions/${id}`, { json: subscription }).json();
+};
+
+export const cancelSubscription = async (id: number): Promise<void> => {
+    await api.delete(`subscriptions/${id}`);
+};
+
+
+
+// Subscription plans
+export const getSubscriptionPlans = async (): Promise<ApiResponse<SubscriptionPlan[]>> => {
+    return await api.get('subscription-plans').json();
+};
+
+export const getSubscriptionPlan = async (id: number): Promise<ApiResponse<SubscriptionPlan>> => {
+    return await api.get(`subscription-plans/${id}`).json();
+};
+
+export const createSubscriptionPlan = async (plan: Partial<SubscriptionPlan>): Promise<ApiResponse<SubscriptionPlan>> => {
+    return await api.post('subscription-plans', { json: plan }).json();
+};
+
+export const updateSubscriptionPlan = async (id: number, plan: Partial<SubscriptionPlan>): Promise<ApiResponse<SubscriptionPlan>> => {
+    return await api.put(`subscription-plans/${id}`, { json: plan }).json();
+};
+
+export const deleteSubscriptionPlan = async (id: number): Promise<void> => {
+    await api.delete(`subscription-plans/${id}`);
+};
---
+++ frontend-admin/src/app/(protected)/subscriptions/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import SubscriptionTable from "@/components/list/subscription/SubscriptionTable";
+
+export default function Subscriptions() {
+    return (
+        <div>
+            <h1>Subscriptions</h1>
+            <SubscriptionTable />
+        </div>
+
+    );
+}
