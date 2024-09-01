1c2190987b3d7e8240ae0a07ba150c8c9288f485
Arvik <94380788+arviik@users.noreply.github.com>
2024-09-01 11:54:08 +0200

    feat(api): added route for users self reset password

--- api/src/api/routes/users.ts
+++ api/src/api/routes/users.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {prisma} from "../../utils/prisma";
-import {userBanValidation, userPatchValidation,} from "../validators/user-validator";
+import {userBanValidation, userPatchValidation, userSelfResetPasswordValidation,} from "../validators/user-validator";
 import {isAuthenticated, isSuperAdmin} from "../middlewares/auth-middleware";
 import {Filter, filterValidator} from "../validators/filter-validator";
 import {createResetPasswordToken} from "../services/email-service";
 import {User} from "@prisma/client";
+import bcrypt from "bcrypt";
+import {revokeTokens} from "../services/auth-services";
 
 export const initUsers = (app: express.Express) => {
     app.get(`/users/me`, isAuthenticated, async (req: any, res) => {
@@ -217,6 +219,45 @@ export const initUsers = (app: express.Express) => {
             });
 
             return res.status(200).json({data: user});
+        } catch (e) {
+            return res.status(500).send({error: e});
+        }
+    });
+
+    app.patch("/users/self-reset-password", isAuthenticated, async (req: any, res) => {
+        try {
+            const validation = userSelfResetPasswordValidation.validate(req.body);
+
+            if (validation.error) {
+                return res.status(400).json({error: validation.error});
+            }
+
+            const userRequest = validation.value;
+            userRequest.newPassword = await bcrypt.hash(userRequest.newPassword, 10);
+
+            let user = await prisma.user.findUnique({where: {id: +req.user.userId}});
+
+            const oldPasswordMatch: boolean = await bcrypt.compare(userRequest.oldPassword, user!.password);
+            if (!oldPasswordMatch) {
+                return res.status(400).json({error: "Error"});
+            }
+
+            const newPasswordMatch: boolean = await bcrypt.compare(userRequest.newPassword, user!.password);
+            if (newPasswordMatch) {
+                return res.status(400).json({error: "New password can't be the same as the old password"});
+            }
+
+            const newPasswordHash = await bcrypt.hash(userRequest.newPassword, 10);
+            await prisma.user.update({
+                where: {id: user!.id},
+                data: {
+                    password: newPasswordHash,
+                }
+            });
+
+            await revokeTokens(user!.id);
+
+            return res.status(200).json({message: "Password updated successfully"});
         } catch (e) {
             return res.status(500).send({error: e});
         }
