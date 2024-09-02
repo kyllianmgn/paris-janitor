681fe8259549c2f1d4132725bc7bf3b7e4858d09
Arvik <94380788+arviik@users.noreply.github.com>
2024-09-02 17:14:34 +0200

    feat(fronted-user): Added support for landlord author in review

--- frontend-user/src/components/review/ReviewElement.tsx
+++ frontend-user/src/components/review/ReviewElement.tsx
@@ -1,12 +1,26 @@
 import {PropertyReview, ServiceReview} from "@/types";
 import {Star} from "lucide-react";
+import {useEffect, useState} from "react";
 
 export const ReviewElement = ({baseReview}: { baseReview: PropertyReview | ServiceReview }) => {
+    const [author, setAuthor] = useState<string>("");
+
+    const loadAuthor = async () => {
+        if ("landlord" in baseReview) {
+            setAuthor(baseReview.landlord?.user?.firstName + " " + baseReview.landlord?.user?.lastName);
+        } else {
+            setAuthor(baseReview.traveler?.user?.firstName + " " + baseReview.traveler?.user?.lastName);
+        }
+    }
+
+    useEffect(() => {
+        loadAuthor().then();
+    })
 
     return (
         <div className="my-2">
             <div className="font-bold">
-                Auteur: {baseReview.traveler?.user?.firstName} {baseReview.traveler?.user?.lastName}
+                Auteur: {author}
             </div>
             <h1 className="font-bold">Note</h1>
             <div className="flex gap-1">
